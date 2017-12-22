de5d2aa128d148c21c3a6a7903c5a08002b31039
sarath kumar <saraths674@gmail.com>
2017-12-22 14:52:15 +0530

    second commit

---
+++ jetty_9.4.8_inspec/controls/control_jetty_9.4.8.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+#
+# Copyright 2017, sarath kumar
+#
+# dual licensed under the Apache License 2.0 and Eclipse Public License 1.0;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.eclipse.org/licenses/
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# author: Sarath kumar
+
+title 'Jetty server config'
+
+control 'jetty-01' do
+  impact 1.0
+  title 'it should be listening'
+  desc 'it should be listening'
+  describe port(8080) do
+  it { should be_listening }
+end
+  end
