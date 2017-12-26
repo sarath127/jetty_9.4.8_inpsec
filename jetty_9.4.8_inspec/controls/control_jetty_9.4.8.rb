52809ee85ad3a46c14d8ba8adaffc7c2072b489e
sarath kumar <saraths674@gmail.com>
2017-12-26 12:19:22 +0530

    after 10 controls

--- jetty_9.4.8_inspec/controls/control_jetty_9.4.8.rb
+++ jetty_9.4.8_inspec/controls/control_jetty_9.4.8.rb
@@ -22,18 +22,48 @@ title 'Jetty server config'
 control 'jetty-01' do
   impact 1.0
   title 'it should be listening'
-  desc 'it should be listening'
+  desc  'it should be listening'
   describe port(8080) do
-  it { should be_listening }
+      it { should be_listening }
+  end
+  describe host('localhost', port: 8080, protocol: 'tcp') do
+      it { should be_reachable }
+      it { should be_resolvable }
+      its('ipaddress') { should include '127.0.0.1' }
+  end
 end
-end
+
 
 control 'jetty-02' do
   impact 1.0
   title 'it should be lnstalled'
-  desc 'it should be installed'
+  desc  'it should be installed'
   describe command('java') do
-  it { should exist }
-end
+      it { should exist }
+  end
+  describe processes('java.exe') do
+      it { should exist }
+  end
 end
 
+
+control 'jetty-03' do
+  impact 1.0
+  title 'checking operating system properties'
+  desc  'checking operating system properties'
+  describe os[:family] do
+      it { should eq 'windows' }
+  end
+  describe os[:release] do
+      it { should eq '10.0.15063' }
+  end
+  describe os[:arch] do
+      it { should eq 'x86_64' }
+  end
+end
+
+=begin
+control 'jetty-04' do
+  impact 1.0
+end
+=end
